5ef88b337faa3971d1ac08606e52f5d704643622
prannoyd <prannoy.tumuluri@gmail.com>
2017-04-03 21:40:55 -0400

    Finally Learnt React

---
+++ Button.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Button = React.createClass({
+	render: function () {
+		return (
+	<button className={ this.props.light ? 'light-button' : 'dark-button'} onClick={this.props.onClick}>
+				Refresh
+			</button>
+		);
+	}
+});
+module.exports = Button;
